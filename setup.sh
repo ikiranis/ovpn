c2616a2a4051b8c68e15181c8ccab52475d24c1c
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-09-12 01:44:02 +0300

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Setup
 docker volume create --name data
-docker run -v data:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://rocean.ddns.net
+docker run -v data:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_genconfig -u udp://my.server.com
 docker run -v data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
 
 # Run container
