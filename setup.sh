351d4bba4f128de489c71b2aa10bfa711873ee0b
Yiannis Kiranis <50238022+ikiranis@users.noreply.github.com>
2019-09-11 23:59:22 +0300

    Update port

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ docker run -v data:/etc/openvpn --log-driver=none --rm kylemanna/openvpn ovpn_ge
 docker run -v data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn ovpn_initpki
 
 # Run container
-docker run -v data:/etc/openvpn -d -p 1195:1195/udp --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v data:/etc/openvpn -d -p 1194:1194/udp --name ovpn --cap-add=NET_ADMIN kylemanna/openvpn
 
 # Generate a client certificate without a passphrase
 docker run -v data:/etc/openvpn --log-driver=none --rm -it kylemanna/openvpn easyrsa build-client-full tardis nopass
